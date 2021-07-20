c0a811dcecf5d4501a69cfb09ccf884ced4cac91
jatibansaljb2001 <62650742+jatibansaljb2001@users.noreply.github.com>
2021-07-20 11:58:41 +0530

    static files

--- myshop/settings.py
+++ myshop/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    '/shop/static/',
-]
+# STATICFILES_DIRS = [
+#     '/shop/static/',
+# ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
